59e33f69a2e9ebd44acee188cc6d151700aa26e7
Joe Dineen <jdineen21@gmail.com>
2019-06-06 17:28:48 +0100

    First Commit

---
+++ importer.py
@@ -0,0 +1,46 @@
+
+import urllib
+import json
+import time
+import csv
+
+import mysql.connector
+import riotApiHandler
+
+while True:
+    featured_games = riotApiHandler.getFeaturedGames()
+
+    for i in range(len(featured_games['gameList'])):
+        game = featured_games['gameList'][i]
+        bannedChampions = game['bannedChampions']
+        gameId = game['gameId']
+        gameLength = game['gameLength']
+        gameMode = game['gameMode']
+        gameQueueConfigId = game['gameQueueConfigId']
+        gameStartTime = game['gameStartTime']
+        gameType = game['gameType']
+        mapId = game['mapId']
+        observers = game['observers']
+        participants = game['participants']
+        platformId = game['platformId']
+
+        for iter in range(len(participants)):
+            riotApiHandler.getSummonerBySummonerName(participants[iter]['summonerName'])['id']
+
+
+    #print participants
+
+    #match = riotApiHandler.getMatchById(gameId)
+    #print match
+
+#print type(featured_games)
+
+#mydb = mysql.connector.connect(
+#    host="localhost",
+#    user="admin",
+#    passwd="badger77"
+#)
+
+#print(mydb)
+
+
---
+++ riotApiHandler.py
@@ -0,0 +1,38 @@
+
+from ratelimit import limits, RateLimitException, sleep_and_retry
+
+import urllib
+import json
+import requests
+
+@sleep_and_retry
+@limits(calls=3, period = 1)
+def getFeaturedGames():
+    r = requests.get('https://euw1.api.riotgames.com/lol/spectator/v4/featured-games?api_key=RGAPI-eb2d12a4-fb4e-4c6c-b621-4b2fa15e0208')
+    #print r.headers
+    if r.status_code == 429:
+        print 'EXCEEDED'
+    else:
+        return r.json()
+
+@sleep_and_retry
+@limits(calls=3, period = 1)
+def getMatchById(matchId):
+    r = requests.get('https://euw1.api.riotgames.com/lol/match/v4/matches/%s?api_key=RGAPI-eb2d12a4-fb4e-4c6c-b621-4b2fa15e0208' % (matchId))
+    #print r.headers
+    if r.status_code == 429:
+        print 'EXCEEDED'
+    else:
+        return r.json()
+
+@sleep_and_retry
+@limits(calls=1, period = 2)
+def getSummonerBySummonerName(summonerName):
+    escapedSummonerName = urllib.quote(summonerName.encode('utf -8'))
+    url = 'https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/' + escapedSummonerName  + '?api_key=RGAPI-eb2d12a4-fb4e-4c6c-b621-4b2fa15e0208'
+    r = requests.get(url)
+    print r.headers
+    if r.status_code == 429:
+        print 'EXCEEDED'
+    else:
+        return r.json()
