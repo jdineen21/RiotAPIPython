16beaaa67cf57bd120f41aa9d27e91ac1a009d14
Joe <jdineen21@gmail.com>
2019-06-06 23:12:52 +0100

    .

--- match.py
+++ match.py
@@ -1,3 +1,10 @@
 
 class match:
-    
+    def __init__(self, matchData):
+        self.gameCreation = matchData['gameCreation']
+        self.gameDuration = matchData['gameDuration']
+        self.gameId = matchData['gameId']
+        self.gameMode = matchData['gameMode']
+        self.gameType = matchData['gameType']
+        self.gameVersion = matchData['gameVersion']
+        self.mapId = matchData['mapId']
